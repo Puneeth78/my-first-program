2b6c308cae6c96dc5459021f4e9ffd9e97abc2a7
Puneeth V <PuneethV78@gmail.com>
2025-08-28 23:56:47 +0530

    added main4.py

---
+++ main5.py
@@ -0,0 +1,12 @@
+# string manupulation
+
+# 1) commom string operatiomn
+
+# first_name=input("enter the first name: ")
+# last_name=input("enter the last name:  ")
+# full_name=first_name + " " + last_name
+# print(full_name)
+
+#  repetition
+message= " puneeth "
+print(message * 10)
