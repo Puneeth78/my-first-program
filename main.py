bc4016ce46b96db66c2ec00302c83285a39eef42
Puneeth V <PuneethV78@gmail.com>
2025-08-29 17:31:33 +0530

    add main6.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# 4. Indexing
+text = "Python"
+print(text[0])   # P
+print(text[-1])  # n
--- main5.py
+++ main5.py
@@ -8,5 +8,38 @@
 # print(full_name)
 
 #  repetition
-message= " puneeth "
-print(message * 10)
+# message= " puneeth "
+# print(message * 10)
+
+# comman string methods in python
+
+# 1) upper() converted into upper case
+# text="hello, world"
+# print(text.upper())  #op HELLO WORLD
+# print(text.lower()) # op hello world
+# print(text.title())  #op Hello World
+# print(text.capitalize()) # Hello world
+# print(text.strip()) # hello world
+# print(text.replace("world","puni")) # hello puni
+# print("-".join("hello world"))
+# print(text.find("wo"))
+# print(text.count("l"))
+# print(text.split(","))
+
+
+# accessing the chareacter
+name="puneeth"
+print(name[0])
+print(name[2])
+print(name[-1]) # negattive value
+print(name[-2])  # negattive value
+
+# string sliceing
+
+text="davangere"
+print(text[0:5])
+print(text[:3])
+print(text[3:])
+print(text[:-3])
+print(text[::-1]) #reversing
+print(text[::2])
