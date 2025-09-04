7e0e398e44db18ab02876d05f5744bef78a142b5
Puneeth V <PuneethV78@gmail.com>
2025-09-04 22:57:34 +0530

    Update main7.py

--- main7.py
+++ main7.py
@@ -1,8 +1,13 @@
+# *) examples for while loop 
+
+# 1) Write a Python program to print numbers from 1 to 10 using a while loop.
+
 i=1
 while i<=10:
     print(i)
     i+=1
 
+ # 2) Write a Python program to print all odd numbers from 1 to 50 using a while loop.
 i=1
 while i<=50:
     if i%2==0:
@@ -11,6 +16,7 @@ while i<=50:
     print(i)
     i+=1
 
+# Write a Python program to print all odd numbers +1 (next number after odd) from 1 to 50 using a while loop.
 i=1
 while i<=50:
     if i%2==0:
@@ -19,6 +25,7 @@ while i<=50:
     i+=1
     print(i)
 
+# Write a Python program to find the sum of first 10 natural numbers using a while loop.
 i=1
 total=0
 while i<=10:
@@ -26,11 +33,14 @@ while i<=10:
     i+=1
 print("total",total)
 
+
+# Write a Python program to print numbers in reverse order starting from a given number.
 num=int(input("enter the  number: "))
 while num>=1:
     print(num)
     num=num-1
 
+# Write a Python program to find the factorial of a number using a while loop.
 n=int(input("enter the number: "))
 fact=1
 i=1
@@ -39,7 +49,7 @@ while i<=n:
     i+=1
 print("factorial numn: ",fact)
 
-
+# Write a Python program to print the multiplication table of a given number using a while loop.
 num=int(input("enter the num: "))
 i=1
 while i<=10:
@@ -47,6 +57,7 @@ while i<=10:
     i+=1
     continue
 
+# Write a Python program to reverse a number using a while loop.
 num=int(input("enter the number: "))
 reverse=0
 while num>0:
@@ -55,7 +66,7 @@ while num>0:
     num=num//10
 print("reversed number is: ",reverse)
 
-
+# Write a Python program to count the number of digits in a given number using a while loop.
 n=int(input("enter the number: "))
 count=0
 while n>0:
@@ -63,6 +74,7 @@ while n>0:
     count+=1
 print("total digit: ",count)
 
+# Write a Python program to check whether a number is a palindrome or not using a while loop.
 n=int(input("enter the number: "))
 temp=n
 rev=0
